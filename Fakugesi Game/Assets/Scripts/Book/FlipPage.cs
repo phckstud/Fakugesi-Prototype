5fb23975a26637898118c85cd027ed59d63f2dcd
LangaNhlanhla <digitalhumansworldwide@gmail.com>
2022-10-17 23:49:05 +0200

    FLip Page Script In + Canvas SetUp

---
+++ Fakugesi Game/Assets/Scripts/Book/FlipPage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class FlipPage : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
+
+/*
+
+Copyright (C) Nhlanhla 'Stud' Langa
+
+*/
