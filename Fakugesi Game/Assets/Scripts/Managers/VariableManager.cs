a460a2455631432a4ab190f77deb516b273db8a2
LangaNhlanhla <digitalhumansworldwide@gmail.com>
2022-10-19 01:41:39 +0200

    Animations Created For Portraits + Update to Exposition

--- Fakugesi Game/Assets/Scripts/Managers/VariableManager.cs
+++ Fakugesi Game/Assets/Scripts/Managers/VariableManager.cs
@@ -9,17 +9,7 @@ public class VariableManager : SingletonPersistent<VariableManager>
 {
     #region Variables
     [Header("Story Specific Strings")]
-    [SerializeField] string location = "locationChosen";
-    [SerializeField] string allies = "numberOfAllies";
-    [SerializeField] string treasure = "treasureWanted";
-    [SerializeField] string difficulty = "difficultyScale";
     [SerializeField] string advance = "levelCanAdvance";
-
-    [Header("Story Specific Integers")]
-    [SerializeField] int locationChosen;
-    [SerializeField] int numberOfAllies;
-    [SerializeField] int treasureWanted;
-    [SerializeField] int difficultyScale;
 
     [Header("Story Specific Boolean")]
     [SerializeField] bool levelCanAdvance;
@@ -32,48 +22,12 @@ public class VariableManager : SingletonPersistent<VariableManager>
 
     public void CheckAndUpdateVariables()
 	{
-        //Update location
-        locationChosen = ((Ink.Runtime.IntValue)DialogueManager.Instance.GetVariableState(location)).value;
-        Debug.Log("Location: " + locationChosen);
-
-        //Update Allies
-        numberOfAllies = ((Ink.Runtime.IntValue)DialogueManager.Instance.GetVariableState(allies)).value;
-        Debug.Log("Allies: " + numberOfAllies);
-
-        //Update treasure
-        treasureWanted = ((Ink.Runtime.IntValue)DialogueManager.Instance.GetVariableState(treasure)).value;
-        Debug.Log("Treasure: " + treasureWanted);
-
-        //Update treasure
-        difficultyScale = ((Ink.Runtime.IntValue)DialogueManager.Instance.GetVariableState(difficulty)).value;
-        Debug.Log("Difficulty: " + difficultyScale);
-
         //Update story boolean
         levelCanAdvance = ((Ink.Runtime.BoolValue)DialogueManager.Instance.GetVariableState(advance)).value;
         Debug.Log("Story Progress: " + levelCanAdvance);
     }
 
 	#region Integer/Boolean Callbacks
-	public int GetLocation()
-	{
-        return locationChosen;
-	}
-
-    public int GetNumberOfAllies()
-	{
-        return numberOfAllies;
-	}
-
-    public int GetTreasure()
-	{
-        return treasureWanted;
-	}
-
-    public int GetDifficultyScale()
-	{
-        return difficultyScale;
-	}
-
     public bool GetStoryProgress()
 	{
         return levelCanAdvance;
--- Fakugesi Game/Assets/Story/Dialogue/Campfire_Scene Opening.ink
+++ Fakugesi Game/Assets/Story/Dialogue/Campfire_Scene Opening.ink
@@ -1,16 +1,16 @@
 INCLUDE ../globals.ink
 
 ~ levelCanAdvance = false
-- Say Mzukulu. What did you learn at school today? #speaker: Grandad #portrait: Curious #layout: left
+- Say Mzukulu. What did you learn at school today? #speaker: Grandad #portrait: GrandadCurious #layout: left
 
 - uhhhhm, I learnt a lot about great civilizations. The Romans, Ancient Greece and Britian
-  #speaker: Granddaughter #portrait: recalling  #layout: right
+  #speaker: Granddaughter #portrait: GranddaughterRecalling  #layout: right
 
 - Ok and what about where we live, any civilizations from Africa?
-- #speaker: Grandad #portrait: curious #layout: left
+- #speaker: Grandad #portrait: GrandadCurious #layout: left
 
--   I learnt about Egypt and…actually that was it. We are the worlds second largest continent though. #speaker: Granddaughter #portrait: brightly #layout:right
+-   I learnt about Egypt and…actually that was it. We are the worlds second largest continent though. #speaker: Granddaughter #portrait: GranddaughterBrightly #layout:right
 
-- There are many great empires from Africa some of them you may never get to hear about. #speaker: Grandad #portrait:Laughing #layout:right
+- There are many great empires from Africa some of them you may never get to hear about. #speaker: Grandad #portrait: GrandaddLaughing #layout:right
 ~ levelCanAdvance = true
 -> END
